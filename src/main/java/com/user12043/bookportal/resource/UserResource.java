e2a91038308c9c256ec775e4e58e37e77fa988c8
user12043 <user12043@hotmail.com>
2021-09-04 22:00:27 +0300

    add /api prefix to rest service, additional app properties

--- src/main/java/com/user12043/bookportal/resource/UserResource.java
+++ src/main/java/com/user12043/bookportal/resource/UserResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserResource {
     private final UserService userService;
 
