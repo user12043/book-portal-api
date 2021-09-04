ecc6cf1c5a1aed634372c4311832ba0113a2449f
user12043 <user12043@hotmail.com>
2021-09-04 13:35:19 +0300

    add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.user12043.bookportal {
+    requires spring.boot;
+    requires spring.context;
+    requires spring.beans;
+    requires spring.data.jpa;
+    requires java.persistence;
+    requires org.hibernate.orm.core;
+    requires spring.boot.autoconfigure;
+    requires spring.web;
+
+    exports com.user12043.bookportal;
+}
